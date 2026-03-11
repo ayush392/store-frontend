78778ae4b1f90d3d17d7b5369c8191dc952d210a
ayush392 <ayushgoyal.0004@gmail.com>
2026-03-12 00:01:00 +0530

    feat: update PWA configuration and icons; remove unused manifest and service worker files; refactor route component

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,60 @@ import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true
     }),
     react(),
-    tailwindcss()
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        navigateFallback: '/index.html'
+      },
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      manifest: {
+        name: 'Dukaan Book',
+        short_name: 'Dukaan',
+        description: 'Dukaan Book application',
+        theme_color: '#2563eb',
+        background_color: '#2563eb',
+        display: 'standalone',
+        start_url: '/',
+        id: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/icons/icon-192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/icons/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ]
 });
